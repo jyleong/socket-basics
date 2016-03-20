1f615ec2d34d2bcba1c8746c92c81d149194f756
James Leong <jyleong@sfu.ca>
2016-03-19 17:09:53 -0700

    disconnect func, currentusers command

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// sends current users to provided socket
+function sendCurrentUsers(socket) {
+	var Info = clientInfo[socket.id]; // client info pulled in
+	var users = [];
+	if (typeof Info == 'undefined') {return;}
+	// search throught clientInfo object
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userinfo = clientInfo[socketId];
+		if (Info.room === userinfo.room) { // if those clients are in same room
+			users.push(userinfo.name);
+		}
+	});
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: now.valueOf()
+	});
+};
+
 io.on('connection',function(socket) {
 	console.log("User connected via socket.io!");
 	socket.on('joinRoom', function(req) {
@@ -23,11 +42,30 @@ io.on('connection',function(socket) {
 			timestamp: now.valueOf()
 		});
 	});
+
+	socket.on('disconnect', function() {
+		var userData = clientInfo[socket.id];
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + " has left",
+				timestamp: now.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('message', function(message) {
 		
-		message.timestamp = now.valueOf();
 		console.log('Message received ' + message.text);
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if(message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		}
+		else {
+			message.timestamp = now.valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 	// both would have timestamp property - JavaScript timestamp in millis
 	socket.emit('message', {
